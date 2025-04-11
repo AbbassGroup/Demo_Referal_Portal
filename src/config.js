67bdbb74767ab342df73bd094ddf3e4dff7ca09a
Abbass Group <webproject@abbass.group>
2025-04-11 13:43:06 +1000

    35th commit

--- src/config.js
+++ src/config.js
@@ -1,8 +1,3 @@
-// Try different API path structures
 const API_URL = 'https://referral-backend-c7os.onrender.com/api';
-// If this doesn't work, try these alternatives:
-// const API_URL = 'https://abbass.group/api';
-// const API_URL = 'https://abbass.group/backend/api';
-// const API_URL = 'https://abbass.group';
 
 export default API_URL;
